02bd83ee24853c103e74cc3083b72016d701cdfc
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2023-09-05 22:00:09 +0530

    Commited

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Main from './OrderFoodModule/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+        </Routes>
+      </BrowserRouter>
       <Main />
     </div>
   );
